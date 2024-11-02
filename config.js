a14e98e38febfccac0f5fbd2946aa33e893aa77f
Tharusndp <sndptharu5@gmail.com>
2024-11-02 08:09:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "f",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "tvczjK7C#sVQQhRFxrOnXKritQRzPHPVsQ5o6I1fOfmactP-iNWc",
+MONGODB: process.env.MONGODB || "mongodb://mongo:KrhFaJbJfKonuiMMuJfJctMHfeXxpINu@junction.proxy.rlwy.net:31135",//enter mongo db url
 };
